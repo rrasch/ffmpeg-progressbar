d9be5e4ba84aff8628c0b9159eedaa44e608bc90
Rasan Rasch <rasan@nyu.edu>
2023-09-08 08:46:42 -0400

    Update route paths

--- progressbar.py
+++ progressbar.py
@@ -12,22 +12,24 @@ db = redis.Redis(decode_responses=True)
 #         g.db = redis.Redis(decode_responses=True)
 #     return g.db
 
+
 @app.teardown_appcontext
 def close_db(e=None):
     # db = g.pop("db", None)
     if db is not None:
         db.close()
 
+
 # from flask_redis import FlaskRedis
 # redis = FlaskRedis()
-# 
+#
 # def create_app():
 #     app = Flask(__name__)
 #     redis.init_app(app, url='redis://localhost:6379/0')
 #     return app
 
 
-@app.route("/progress/<video>/<duration>", methods=["POST"])
+@app.route("/update/<video>/<duration>", methods=["POST"])
 def update_progress(video, duration):
     # db = get_db()
     db.hset(video, "duration", duration)
@@ -41,20 +43,23 @@ def update_progress(video, duration):
             db.expire(video, 60)
     return "Chunks printed successfully"
 
+
 def progress_dict():
     progress = {}
     for video in db.scan_iter("*"):
         logging.debug(f"video: {video}")
-        data =  db.hgetall(video)
+        data = db.hgetall(video)
         data["percent"] = int(int(data["frame"]) / int(data["duration"]) * 100)
         progress[video] = data
     return progress
+
 
 @app.route("/")
 def index():
     return render_template("index.html", progress=progress_dict())
 
-@app.route("/progress")
+
+@app.route("/data")
 def progress():
     return jsonify(progress=progress_dict())
 
@@ -62,5 +67,5 @@ def progress():
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
-    app.run(debug=True)
-
+    app.run(port=5000, debug=True)
+    # app.run(port=5443, ssl_context="adhoc", debug=True)
